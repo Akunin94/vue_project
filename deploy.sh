45b7f73c32712e08b44a53c6b2cf92917042efab
Davron Usmonov <akunin94@gmail.com>
2021-12-26 22:34:59 +0500

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,22 @@
 set -e
 
 # build
-mkdir docs
 npm run build
 
 # navigate into the build output directory
-# cd docs
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Akunin94/vue_project.git master:gh-pages
+git push -f git@github.com:Akunin94/vue_project.git master:gh-pages
 
-# cd -
+cd -
